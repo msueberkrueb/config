1e02af929a7a09b331796231df5d3b8fa09b6acf
msueberkrueb <mike.sueberkrueb@freenet.ag>
2024-03-25 16:32:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Requirements
+
+To use home-manager you need to install the nix cli:
+
+```
+nix (curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install)
+```
+
+# Installation
+
+Installing the config into the environment can be done with the following command:
+
+´´´
+home-manager switch
+´´´
---
+++ home.nix
@@ -0,0 +1,75 @@
+{ config, pkgs, ... }:
+
+{
+    home.username = "msueberkrueb";
+    home.homeDirectory = "/home/msueberkrueb";
+    home.stateVersion = "23.11";
+
+    programs.neovim = {
+        enable = true;
+
+        viAlias = true;
+        vimAlias = true;
+        vimdiffAlias = true;
+
+        plugins = with pkgs.vimPlugins; [
+            {
+                plugin = catppuccin-nvim;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/catppuccin.lua;
+            }
+            {
+                plugin = vim-fugitive;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/fugitive.lua;
+            }
+            {
+                plugin = harpoon;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/harpoon.lua;
+            }
+            {
+                plugin = nvim-lspconfig;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/lsp.lua;
+            }
+            {
+                plugin = telescope-nvim;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/telescope.lua;
+            }
+            {
+                plugin = nvim-treesitter;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/treesitter.lua;
+            }
+            {
+                plugin = undotree;
+                type = "lua";
+                config = builtins.readFile ./nvim/plugins/undotree.lua;
+            }
+        ];
+
+        extraLuaConfig = ''
+            ${builtins.readFile ./nvim/remap.lua}
+            ${builtins.readFile ./nvim/set.lua}
+        '';
+    };
+
+    programs.tmux = {
+        enable = true;
+
+        plugins = with pkgs.tmuxPlugins; [
+            {
+                plugin = catppuccin;
+                extraConfig = builtins.readFile ./tmux/plugins/catppuccin.conf;
+            }
+        ];
+
+        extraConfig = ''
+            ${builtins.readFile ./tmux/tmux.conf}
+        '';
+    };
+
+    programs.home-manager.enable = true;
+}
