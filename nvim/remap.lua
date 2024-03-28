cb60d74e406415e61e4973fa89143e8dd8fc72e7
msueberkrueb <mike.sueberkrueb@freenet.ag>
2024-03-28 10:05:11 +0100

    feat: reevaluate remaps

--- nvim/remap.lua
+++ nvim/remap.lua
@@ -1,31 +1,17 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.g.mapleader = " " -- Set leader
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+-- Remaps
+vim.keymap.set({ "i", "v" }, "öö", "<Esc>") -- Exit current mode
 
-vim.keymap.set("n", "J", "mzJ`z")
-vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
-vim.keymap.set("n", "n", "nzzzv")
-vim.keymap.set("n", "N", "Nzzzv")
+-- Commands
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- Open file explorer
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format) -- Format document
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]]) -- Yank into system clipboard
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]]) -- Delete into void register
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- Substitute current word
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") -- Move current line one down
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv") -- Move current line one up
 
-vim.keymap.set("x", "<leader>p", [["_dP]])
-
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-vim.keymap.set({"n", "v"}, "<leader>d", [["_d]])
-
-vim.keymap.set("i", "<C-c>", "<Esc>")
-vim.keymap.set("i", "öö", "<Esc>")
-
-vim.keymap.set("n", "Q", "<nop>")
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
-vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
-vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
-vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
-vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
-
-vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+-- Movement
+vim.keymap.set("n", "<C-d>", "<C-d>zz") -- Jump down and center view
+vim.keymap.set("n", "<C-u>", "<C-u>zz") -- Jump up and center view
