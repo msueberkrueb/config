32db8aadfc9708c295538bdc480dcacaf020741e
msueberkrueb <mike.sueberkrueb@freenet.ag>
2024-04-11 15:08:51 +0200

    feat: remove host from ps1

--- home.nix
+++ home.nix
@@ -23,7 +23,7 @@
         bash = {
             enable = true;
             bashrcExtra = ''
-                PS1='\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$'
+                PS1='\[\033[01;32m\]\u\[\033[01;34m\]\w\[\033[00m\]\$'
             '';
             shellAliases = {
                 ssh = "ssh.exe";
