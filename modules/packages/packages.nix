f67220c69be08b53972261084b5578f4b856af2d
msueberkrueb <88376203+msueberkrueb@users.noreply.github.com>
2025-05-05 17:15:49 +0200

    feat: add networking packages

--- modules/packages/packages.nix
+++ modules/packages/packages.nix
@@ -1,13 +1,16 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     alejandra
+    dig
     dotnet-sdk
     ilspycmd
     kubectl
     kubelogin-oidc
+    nettools
     nodejs_22
     prettierd
     ripgrep
+    traceroute
     xdg-utils
   ];
 }
