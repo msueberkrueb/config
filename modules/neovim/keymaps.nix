6a85dd5f22dc05346d835c46ecfb392bb8cd78c5
msueberkrueb <88376203+msueberkrueb@users.noreply.github.com>
2025-05-21 07:47:33 +0200

    feat: add vim keybindings for lsp commands

--- modules/neovim/keymaps.nix
+++ modules/neovim/keymaps.nix
@@ -4,5 +4,17 @@
       action = "<cmd>Ex<CR>";
       key = "<leader>e";
     }
+    {
+      action = "<cmd>lua vim.diagnostic.open_float()<CR>";
+      key = "<leader>d";
+    }
+    {
+      action = "<cmd>lua vim.lsp.buf.definition()<CR>";
+      key = "gd";
+    }
+    {
+      action = "<cmd>lua vim.lsp.buf.declaration()<CR>";
+      key = "gD";
+    }
   ];
 }
